0841568da73b494404b649c5d13776247bff795e
Ramesh <ramesh.mavuluri.info@gmail.com>
2021-01-13 22:00:57 -0500

    learning react and coded first sample component :-)

---
+++ src/Toolbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+class Toolbar extends React.Component {
+    render() {
+      return (
+        <div>
+            <h1>Tool bar here</h1>
+        </div>
+      ) 
+    }
+
+}
+
+export default Toolbar;
